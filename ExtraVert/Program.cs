0b9ac66e664580fa5a8cb1f4ba5ed82ba11f3f0c
dimitricbrown <dimitricbrown@outlook.com>
2023-07-17 17:29:11 -0500

    Add project files.

---
+++ ExtraVert/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExtraVert;
+using System.IO.Compression;
+
+List<Plant> plants = new List<Plant>()
+{
+    new Plant()
+    {
+        Species  = "Rose",
+        LightNeeds = 4,
+        AskingPrice = 15,
+        City = "Memphis",
+        ZIP = 38125,
+        Sold = false,
+    },
+    new Plant()
+    {
+        Species  = "Daisy",
+        LightNeeds = 3,
+        AskingPrice = 15,
+        City = "Memphis",
+        ZIP = 38125,
+        Sold = false,
+    },
+    new Plant()
+    {
+        Species  = "Sunflower",
+        LightNeeds = 5,
+        AskingPrice = 15,
+        City = "Memphis",
+        ZIP = 38125,
+        Sold = false,
+    },
+    new Plant()
+    {
+        Species  = "Tulip",
+        LightNeeds = 3,
+        AskingPrice = 15,
+        City = "Memphis",
+        ZIP = 38125,
+        Sold = false,
+    },
+    new Plant()
+    {
+        Species  = "Lily",
+        LightNeeds = 2,
+        AskingPrice = 15,
+        City = "Memphis",
+        ZIP = 38125,
+        Sold = true,
+    }
+};
+
+foreach(var plant in plants)
+{
+    Console.WriteLine($"{plants.IndexOf(plant) + 1}. {plant.Species}");
+}
